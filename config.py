16a472923c561c8bf30313d88edb80fc95e7a3ea
Elian Ahmar <elianahmar@Elians-MacBook-Pro.local>
2024-12-29 16:19:57 -0600

    added schema.prisma file and config file

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv(override=True)  # override option ensure System Env variables are overriden
+
+#WARN: if I'm seeing conflicts with other environment variables during local development just use a json config file
+# All .env variables
+BDOOR = os.environ["BDOOR"]
