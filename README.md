183f1a9268d76d0e88edff02662f775ff9a12335
Elian Ahmar <elianahmar@Elians-MacBook-Pro.local>
2024-12-29 15:49:56 -0600

    added latency header to handlers for performance tracking

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # affix
+1. Git clone this following repository
+2. Create a virtual environment using `$python3 -m venv .venv`
+3. Install requirements using `$pip install -r requirements.txt`
+4. Test to see if the server is up and running using `fastapi dev server/server.py`
--- server/server.py
+++ server/server.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import time
 
 app = FastAPI()
 
@@ -7,3 +8,15 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/hello/{name}")
+async def hello(name: str):
+    return {"name": name, "greeting": f"hello {name}, how ya doing?"}
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    process_time = (time.perf_counter() - start_time) * 1000 #milliseconds
+    response.headers["X-Process-Time"] = str(f"{process_time} ms")
+    return response
