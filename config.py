cb683c19906774b9eeefc31a44b1407ab4f5f744
Elian Ahmar <elianahmar@Elians-MacBook-Pro.local>
2025-01-02 13:09:17 -0600

    saving work. Added error class for exception handling in server

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ load_dotenv(override=True)  # override option ensure System Env variables are ov
 
 #WARN: if I'm seeing conflicts with other environment variables during local development just use a json config file
 # All .env variables
-BDOOR = os.environ["BDOOR"]
+DEV_KEY = os.environ["DEV_KEY"]
 OPENAI_KEY = os.environ["OPENAI_KEY"]
 AFFIX_DB_URL = os.environ["AFFIX_DB_URL"]
---
+++ server/error.py
@@ -0,0 +1,9 @@
+class ErrorHandle:
+    def __init__(self, route: str, code: str, msgs: list[str], trace):
+        self.route = route
+        self.code = code
+        self.msgs = msgs
+
+    #TODO: add error codes and msgs
+
+    
